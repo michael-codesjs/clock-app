0e3ebfafdbdaecee8d2a52abe08f115c54dc0555
Michael Phiri <michael.codesjs@gmail.com>
2022-07-10 21:44:27 +0200

    test fix: css logical bugs for mobile devices(1)

--- src/App.tsx
+++ src/App.tsx
@@ -6,16 +6,14 @@ import Routes from './Routes';
 
 function App() {
   return (
-    <div className='h-screen w-screen'>
-      <div className="h-full w-full flex justify-center items-center overflow-hidden">
-        <div className="sm:max-w-[400px] h-full sm:max-h-[700px] p-5 sm:rounded-3xl w-full flex flex-col items-end bg-gray-50">
-          <div className='w-full h-full'>
-            <Routes />
-          </div>
-          <Navigation />
+    <div className="h-full min-h-full box-border w-full flex justify-center items-center overflow-hidden">
+      <div className="sm:max-w-[400px] h-full sm:max-h-[700px] p-5 sm:rounded-3xl w-full flex flex-col items-end bg-gray-50">
+        <div className='w-full h-full'>
+          <Routes />
         </div>
-        <Toast />
+        <Navigation />
       </div>
+      <Toast />
     </div>
   );
 }
